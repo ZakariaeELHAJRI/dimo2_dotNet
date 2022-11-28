afd688a1a1c27f85c6e72dc4ab72381cbbaffa19
zakariae_elhajri <zakriaelhajri@gmail.com>
2022-11-28 11:45:53 +0100

    first commit zaki

---
+++ Downloads/DOTNET_DEMO2/DOTNET_DEMO/sqlqueries/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿DRop Table produit;
+CREATE TABLE produit (
+    id INT NOT NULL PRIMARY KEY IDENTITY,
+    nom VARCHAR (100) NOT NULL UNIQUE,
+    description VARCHAR (150) NOT NULL,
+    prix INT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO produit (nom, description, prix)
+VALUES
+('Table', ' ', '1570'),
+('Chaise', ' ', '650'),
+('Bureau', ' ', '1200'),
+('Armoire', ' ', '2500'),
+('Etagier', ' ', '250'),
+('coussin', ' ', '60');
